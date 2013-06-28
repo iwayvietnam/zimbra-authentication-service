1e1b115153e21588578d9488b6daf2fc2a8908ae
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-06-28 11:45:04 +0700

    Forward user's info to certifier server

--- idp/server.js
+++ idp/server.js
@@ -1,10 +1,15 @@
 var express = require('express')
+  , request = require('request')
   , url = require('url')
   , path = require('path')
   , fs = require('fs');
 //  , urlparse = require('urlparse');
 
 var app = express();
+
+app.use(express.logger());
+
+app.use(express.bodyParser());
 
 app.use(function(req, res, next) {
   if (req.path === '/.well-known/browserid') {
@@ -13,7 +18,10 @@ app.use(function(req, res, next) {
   next();
 });
 
-
+app.post('/api/cert_key', function (req, res) {
+  var certRequest = request.post('http://localhost:8080/cert_key').form(req.body);
+  certRequest.pipe(res);
+});
 
 app.use(express.static(__dirname + '/static'));
 
