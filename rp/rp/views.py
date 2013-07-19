1de52a0d70a50f3f209f729ab5037f7cdf5597c1
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-07-19 15:12:45 +0700

    Refactor the RP controller code

--- rp/rp/views.py
+++ rp/rp/views.py
@@ -10,29 +10,33 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
 
-# Hainp: Change this to real preauth key
-preauth_key = "0f6f5bbf7f3ee4e99e2d24a7091e262db37eb9542bc921b2ae4434fcb6338284"
-# Hainp: Change this to real zimbra mail server url
-preauth_url = "http://aurl.org"
-# Hainp
+
+zimbra_url = "https://iway.dyndns.org:8001"
+preauth_url = zimbra_url + "/service/preauth"
+mail_url = zimbra_url + "/mail"
 
 def status(request):
-    email = request.session.get('email') or request.user.email
+    if request.session.get('email'):
+        return HttpResponseRedirect(mail_url)
 
-    if email:
-        url = request.session.get('url')
-        if not url:
-            url = generate(email)
-            request.session['url'] = url
-            
+    if request.user.email:
+        email = request.user.email
+
+        # domain_key should be obtained dynamically
+        domain_key = "6b445ab7b53c432eaf1f7e00e82b68953688816ccb895f035f66f69fc206b3d2"
+        
+        url = generate(email, domain_key)
+        
+        request.session['url'] = url
         request.session['email'] = email
+        
         return HttpResponseRedirect(url)
         
     return HttpResponseRedirect('/')
 
 
-def generate(email):
+def generate(email, domain_key):
     timestamp = int(time()*1000)
     acct = email
-    pak = hmac.new(preauth_key, '%s|name|0|%s'%(acct, timestamp), hashlib.sha1).hexdigest()
+    pak = hmac.new(domain_key, '%s|name|0|%s'%(acct, timestamp), hashlib.sha1).hexdigest()
     return "%s?account=%s&expires=0&timestamp=%s&preauth=%s"%(preauth_url, acct, timestamp, pak)    
